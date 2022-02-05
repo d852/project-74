409447264ff1ca7d71da0139f59824d1d2ea308e
d852 <77046880+d852@users.noreply.github.com>
2022-02-05 14:11:08 +0530

    Update RideHistory.js

--- screens/RideHistory.js
+++ screens/RideHistory.js
@@ -243,7 +243,7 @@ const styles = StyleSheet.create({
     fontFamily: "Rajdhani_600SemiBold"
   },
   date: {
-    fontSize: 12,
+    fontSize: 13,
     fontFamily: "Rajdhani_600SemiBold",
     paddingTop: 5
   }
